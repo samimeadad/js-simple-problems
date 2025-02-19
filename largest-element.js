63d8192a3489cdc4a020e718014577c8f16a04a0
Sami Meadad Choudhury <sami.meadad@gmail.com>
2025-02-19 22:22:11 +0600

    largest-element-js modified

--- largest-element.js
+++ largest-element.js
@@ -1,46 +1,46 @@
-function findLargest(arr){
-    let largest = arr[0];
+function findLargest ( arr ) {
+    let largest = arr[ 0 ];
     let len = arr.length;
-    for(let i = 0; i < len; i++){
-        if(arr[i] > largest){
-            largest = arr[i];
+    for ( let i = 0; i < len; i++ ) {
+        if ( arr[ i ] > largest ) {
+            largest = arr[ i ];
         }
     }
     return largest;
 }
 
-function findSmallest(arr){
-    let smallest = arr[0];
+function findSmallest ( arr ) {
+    let smallest = arr[ 0 ];
     let len = arr.length;
-    for(let i = 0; i < len; i++){
-        if(arr[i] < smallest){
-            smallest = arr[i];
+    for ( let i = 0; i < len; i++ ) {
+        if ( arr[ i ] < smallest ) {
+            smallest = arr[ i ];
         }
     }
     return smallest;
 }
 
-function findAvg(arr){
+function findAvg ( arr ) {
     let average = 0;
     let sum = 0;
     let len = arr.length;
-    for(let i = 0; i < len; i++){
-        sum += arr[i];
+    for ( let i = 0; i < len; i++ ) {
+        sum += arr[ i ];
     }
-    return sum/len;
+    return sum / len;
 }
 
-function main(){
-    let myArr = [-76, -78, -98, -72, -87, -89, -85, -84, -90, -87, -96, -93];
-    
-    let myLargest = findLargest(myArr);
-    console.log("Largest Number: ", myLargest);
+function main () {
+    let myArr = [ 76, 78, 98, -72, -87, -89, -85, 84, 90, 87, 96, 93 ];
 
-    let mySmallest = findSmallest(myArr);
-    console.log("Smallest Number: ", mySmallest);
+    let myLargest = findLargest( myArr );
+    console.log( "Largest Number: ", myLargest );
 
-    let myAvg = findAvg(myArr);
-    console.log("Average: ", myAvg);
+    let mySmallest = findSmallest( myArr );
+    console.log( "Smallest Number: ", mySmallest );
+
+    let myAvg = findAvg( myArr );
+    console.log( "Average: ", myAvg );
 }
 
 main();
